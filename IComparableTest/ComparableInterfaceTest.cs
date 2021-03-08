baf70853be10723cadf3c2a430e8f3839ed46cc2
Andrey Topchu <andreytopchu01@gmail.com>
2021-03-08 23:16:26 +0200

    Add new Clean Project

--- IComparableTest/ComparableInterfaceTest.cs
+++ IComparableTest/ComparableInterfaceTest.cs
@@ -7,7 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ComparableInterfaceTest
     {
         [TestMethod]
         public void TestMethod1()
