d70f3d72cf937301ad4642319aef8143b8886cca
Andrey Topchu <andreytopchu01@gmail.com>
2021-03-22 16:27:55 +0200

    Завершена работа над тестированием UniqueCollection.cs Доработан UniqueCollection.cs

--- ObjectsLib/UniqueCollection.cs
+++ ObjectsLib/UniqueCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Text;
 
 namespace ObjectsLib
 {
-    public class UniqueCollection<T>
+    public class UniqueCollection<T>:ICollection<T>
     {
         private Collection<T> _collection;
         
@@ -15,6 +16,10 @@ namespace ObjectsLib
             _collection = new Collection<T>();
         }
 
+        public int Count => ((ICollection<T>)_collection).Count;
+
+        public bool IsReadOnly => ((ICollection<T>)_collection).IsReadOnly;
+
         public void Add(T data)
         {
             if (data == null) throw new ArgumentNullException("Ошибка! Невозможно добавить в коллекцию элемент равный null.");
@@ -22,6 +27,36 @@ namespace ObjectsLib
             if (!_collection.Contains(data))
                 _collection.Add(data);
             else throw new ArgumentException("Невозможно добавить элемент! Данный элемент уже существует в коллекции.");
+        }
+
+        public void Clear()
+        {
+            ((ICollection<T>)_collection).Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return ((ICollection<T>)_collection).Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            ((ICollection<T>)_collection).CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((ICollection<T>)_collection).GetEnumerator();
+        }
+
+        public bool Remove(T item)
+        {
+            return ((ICollection<T>)_collection).Remove(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((ICollection<T>)_collection).GetEnumerator();
         }
     }
 }
--- Tests/UniqueCollectionTest.cs
+++ Tests/UniqueCollectionTest.cs
@@ -1,14 +1,96 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObjectsLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Tests
 {
     [TestClass]
     public class UniqueCollectionTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        public bool CheckForNotRepetInTheCollection<T>(UniqueCollection<T> uniqueCollection)
         {
+            HashSet<T> hs = new HashSet<T>();
+            foreach (var item in uniqueCollection)
+            {
+                try
+                {
+                    hs.Add(item);
+                }
+                 catch(ArgumentException)
+                {
+                    Console.WriteLine("Коллекция не уникальна. В ней найдены два одинаковых элемента.");
+                    return false;
+                }
+            }
+            return true;
         }
+
+        [TestMethod]
+        public void GenerationUniqueCollectionWith10000UniquePersonsTest()
+        {
+            int countOfElements = 10000;
+            UniqueCollection<Person> uniqueCollection = new UniqueCollection<Person>();
+
+            for (int i = 0; i < countOfElements; i++)
+            {
+                uniqueCollection.Add(GeneratorOfPersons.GeneratePerson());
+            }
+
+            int actual = uniqueCollection.Count;
+            int expected = countOfElements;
+            Assert.AreEqual(expected, actual);
+
+            Assert.IsTrue(CheckForNotRepetInTheCollection<Person>(uniqueCollection));
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void GenerationUniqueCollectionWith100NotUniquePersonsTest()
+        {
+            int countOfElements = 100;
+            UniqueCollection<Person> uniqueCollection = new UniqueCollection<Person>();
+
+            for (int i = 0; i < countOfElements; i++)
+            {
+                uniqueCollection.Add(GeneratorOfPersons.GeneratePerson());
+            }
+
+            Person[] uniqueCollectionItems = new Person[uniqueCollection.Count];
+            uniqueCollection.CopyTo(uniqueCollectionItems, 0);
+            uniqueCollection.Add(uniqueCollectionItems[countOfElements / 2]);
+
+            int actual = uniqueCollection.Count;
+            int expected = countOfElements;
+            Assert.AreEqual(expected, actual);
+
+            CollectionAssert.AllItemsAreUnique((ICollection)uniqueCollection);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void GenerateUniqueCollectionWithNotUniqueDoubleItemsTest()
+        {
+            UniqueCollection<double> uniqueCollection = new UniqueCollection<double>();
+
+            uniqueCollection.Add(12.12);
+            uniqueCollection.Add(5.5);
+            uniqueCollection.Add(6.3);
+            uniqueCollection.Add(2.2);
+            uniqueCollection.Add(5.5);
+
+            List<double> list = new List<double>() { 12.12, 5.5, 6.3, 2.2 };
+
+            int actual = uniqueCollection.Count;
+            int expected = list.Count;
+            Assert.AreEqual(expected, actual);
+
+            CollectionAssert.AreEqual((ICollection)uniqueCollection, list);
+            CollectionAssert.AllItemsAreUnique((ICollection)uniqueCollection);
+        }
+
+
     }
 }
